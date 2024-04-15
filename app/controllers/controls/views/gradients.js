63f25f53492e984f821fa9521f3b8a2b90d143ad
Cesar Estrada <cesar@codigomovil.mx>
2024-04-15 08:47:40 -0500

    rafactoring code

--- app/controllers/controls/views/gradients.js
+++ app/controllers/controls/views/gradients.js
@@ -1,11 +1,11 @@
 import { logger } from 'logger'
 
-let normalizedCenterX
-let normalizedCenterY
 let colors
 let Gradient
 let gradientTypes
-let selectedGradient;
+let selectedGradient
+let normalizedCenterX
+let normalizedCenterY;
 
 /**
  * The scoped constructor of the controller.
@@ -27,45 +27,52 @@ function updateGradient() {
     y: size.height * normalizedCenterY,
   }
 
+  const gradient = {
+    type: gradientTypes[selectedGradient],
+    colors: selectedGradient === Gradient.LINEAR
+      ? [{ color: colors[0], offset: 0.0 }, { color: colors[1], offset: 1.0 }]
+      : colors,
+  }
+
+  if (selectedGradient === Gradient.LINEAR) {
+    updateLinearGradient(gradient)
+  } else {
+    updateRadialGradient(gradient, centerPoint, minDimension)
+  }
+
+  $.gradientView.applyProperties({ backgroundGradient: gradient })
+}
+
+function updateLinearGradient(gradient) {
+  const startPoint = { x: precisionRound($.startRadiusSlider.value, -1) + '%', y: '50%' }
+  const endPoint = { x: precisionRound($.endRadiusSlider.value, -1) + '%', y: '50%' }
+
+  Object.assign(gradient, { startPoint, endPoint })
+
+  logger.log(`Linear gradient updated: start-point: ${JSON.stringify(startPoint)}, end-point: ${JSON.stringify(endPoint)}`)
+}
+
+function updateRadialGradient(gradient, centerPoint, minDimension) {
   const startRadius = (minDimension / 2) * ($.startRadiusSlider.value / 100)
   const endRadius = (minDimension / 2) * ($.endRadiusSlider.value / 100)
 
-  const gradient = {
-    type: gradientTypes[selectedGradient]
-  }
+  Object.assign(gradient, {
+    startPoint: centerPoint,
+    endPoint: centerPoint,
+    startRadius: startRadius,
+    endRadius: endRadius,
+    backfillStart: $.startFillSwitch.value,
+    backfillEnd: $.endFillSwitch.value
+  })
 
-  // Linear gradients support colors with offsets and start-point / end-point
-  // Radial gradients support raw colors, start-radius / end-radius and backfill-start / backfill-end
-  // Read more: https://titaniumsdk.com/api/titanium/ui/view.html#backgroundgradient
-  if (selectedGradient === Gradient.LINEAR) {
-    const startPoint = { x: precisionRound($.startRadiusSlider.value, -1) + '%', y: '50%' }
-    const endPoint = { x: precisionRound($.endRadiusSlider.value, -1) + '%', y: '50%' }
-
-    gradient.colors = [{ color: colors[0], offset: 0.0 }, { color: colors[1], offset: 1.0 }]
-    gradient.startPoint = startPoint
-    gradient.endPoint = endPoint
-
-    logger.log('Linear gradient updated: ' + JSON.stringify(centerPoint) + ', start-point: ' + JSON.stringify(startPoint) + ', end-point: ' + JSON.stringify(endPoint))
-  } else {
-    gradient.startPoint = centerPoint
-    gradient.endPoint = centerPoint
-    gradient.startRadius = startRadius
-    gradient.endRadius = endRadius
-    gradient.backfillStart = $.startFillSwitch.value
-    gradient.backfillEnd = $.endFillSwitch.value
-    gradient.colors = colors
-
-    logger.log('Radial gradient updated: ' + JSON.stringify(centerPoint) + ', start-radius: ' + startRadius + ', end-radius: ' + endRadius)
-  }
-
-  $.gradientView.backgroundGradient = gradient
+  logger.log(`Radial gradient updated: ${JSON.stringify(centerPoint)}, start-radius: ${startRadius}, end-radius: ${endRadius}`)
 }
 
 function handleTouchMove(event) {
-  const size = $.gradientView.rect
+  const { width, height } = $.gradientView.rect
 
-  normalizedCenterX = (size.width > 0) ? (event.x / size.width) : 0.5
-  normalizedCenterY = (size.height > 0) ? (event.y / size.height) : 0.5
+  normalizedCenterX = width > 0 ? event.x / width : 0.5
+  normalizedCenterY = height > 0 ? event.y / height : 0.5
 
   updateGradient()
 }
@@ -76,30 +83,24 @@ function pickRandomColor() {
 }
 
 // CREDITS: https://stackoverflow.com/a/1484514/5537752
-
 function generateRandomColor() {
-  const letters = '0123456789ABCDEF'
-  let color = '#'
-
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)]
-  }
-
-  return color
+  return '#' + Array.from({ length: 6 }, () => Math.floor(Math.random() * 16).toString(16)).join('')
 }
 
 function handleGradientType({ index }) {
   selectedGradient = index
-  let isLinear = (index === Gradient.LINEAR)
-  $.startFillSwitch.enabled = !isLinear
-  $.endFillSwitch.enabled = !isLinear
-  $.startRadiusLabel.text = isLinear ? 'Start Point:' : 'Start Radius:'
-  $.endRadiusLabel.text = isLinear ? 'End Point' : 'End Radius'
+  const isLinear = (index === Gradient.LINEAR)
+
+  $.startFillSwitch.applyProperties({ enabled: !isLinear })
+  $.endFillSwitch.applyProperties({ enabled: !isLinear })
+
+  $.startRadiusLabel.applyProperties({ text: isLinear ? 'Start Point:' : 'Start Radius:' })
+  $.endRadiusLabel.applyProperties({ text: isLinear ? 'End Point' : 'End Radius' })
 
   updateGradient()
 }
 
 function precisionRound(number, precision) {
-  var factor = Math.pow(10, precision)
+  const factor = Math.pow(10, precision)
   return Math.round(number * factor) / factor
 }
